0ab6174f64bdfe699f1e3f9289c82e8cc23a403d
Skye Im <skye.im@nyu.edu>
2018-12-08 16:42:25 -0500

    Minio serves files

--- backend/thumbnail/minio-init.sh
+++ backend/thumbnail/minio-init.sh
@@ -3,4 +3,13 @@
 mkdir -p data/thumb
 mkdir -p data/image
 
-minio server /data
+minio server /data &
+
+# Set policy
+wget https://dl.minio.io/client/mc/release/linux-amd64/mc
+chmod +x mc
+./mc config host add minio http://localhost:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+./mc policy download minio/image/
+./mc policy download minio/thumb/
+
+wait
