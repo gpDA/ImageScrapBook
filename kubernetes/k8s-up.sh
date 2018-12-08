3b9d1497f4558d0ce5aaed45dbf681a1a5667126
Skye Im <skye.im@nyu.edu>
2018-12-07 23:28:29 -0500

    Finish deployment configs

--- kubernetes/k8s-up.sh
+++ kubernetes/k8s-up.sh
@@ -1,14 +1,16 @@
 # Foundational services
 kubectl create -f minio-deployment.yaml
 kubectl create -f rabbit-deployment.yaml
+kubectl create -f postgres-deployment.yaml
 kubectl create -f minio-service.yaml
 kubectl create -f rabbit-service.yaml
+kubectl create -f postgres-service.yaml
 
 # Client services
 kubectl create -f celeryworker-deployment.yaml
 kubectl create -f flowerbox-deployment.yaml
-kubectl create -f workprovider-deployment.yaml
+kubectl create -f appserver-deployment.yaml
 
 # Expose to outside world
 kubectl create -f flowerbox-service.yaml
-kubectl create -f minio-service.yaml
+kubectl create -f appserver-service.yaml
