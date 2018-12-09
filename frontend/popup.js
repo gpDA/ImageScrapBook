8195d36a99e7b06acf67fa68d12560df640030f9
estherkim304 <syk460@nyu.edu>
2018-12-09 13:22:25 -0500

    Add files via upload

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,12 +1,15 @@
-/**
-	This popup.js file is used to receive the list of urls that the grabber content script has found
-  and show them to the popup.html file.
-**/
+// This popup.js file is the receive all the image urls data grabbed from the grabbed.js
+// and show them on the popup.html
 
 function init() {
 	go_gallery();
 	go_public_gallery();
-	startLoading();
+	//grab_img();
+}
+
+
+function startLoading(){
+  chrome.extension.getBackgroundPage().console.log("Popup clicked!");
 }
 
 function get_urls() {
@@ -18,21 +21,33 @@ function get_urls() {
     wholeList.appendChild(listElement);
     for (var i = 0; i < picUrls.length; i++){
       var listItem = document.createElement("li");
-      listItem.innerHTML = "<a href= save_img.html><img src=" + picUrls[i].src + " width=50%, height=50%>" + "</a>";
+      listItem.innerHTML = "<a href= save_img.html><img src=" + picUrls[i].src + " width=30%, height=30%>" + "</a>";
       chrome.extension.getBackgroundPage().console.log( "src: ", picUrls[i].src );
       listElement.appendChild(listItem);
     }
     chrome.extension.getBackgroundPage().console.log("get_urls function ");
   }
   else{
+		console.log("Images unavailable");
     document.body.innerHTML = "Images unavilable";
   }
 }
+// when the page loads, show the images!
+// onload:  used within the <body> element to execute a script once a
+//      web page has **completely loaded** all content (including images, script files, CSS files, etc.).
+window.onload = get_urls();
+
+//=============BUTTONS on HTML===============
+
+//TODO: direct to my gallery/public gallery
+var newURL = "http://google.com";
 
 function go_public_gallery(){
 	var pg_btn = document.getElementById('public_gallery_btn');
 	pg_btn.addEventListener('click', function() {
 			chrome.extension.getBackgroundPage().console.log("open publilc gallery");
+			//open the public gallery page
+			chrome.tabs.create({url: newURL});
 	});
 }
 
@@ -40,31 +55,10 @@ function go_gallery(){
 	var g_btn = document.getElementById('my_gallery_btn');
 	g_btn.addEventListener('click', function() {
 			chrome.extension.getBackgroundPage().console.log("open my gallery");
+			//open the my gallery page
+			chrome.tabs.create({url: newURL});
 	});
 }
-
-function startLoading(){
-  //start loading the rest of the script
-  chrome.extension.getBackgroundPage().console.log("popup clicked!");
-  get_urls();
-}
-
-// when the page loads, remove the gif and execute the rest
-//window.onload = startLoading()
-
-
-
-
-// tracking code for analytics
-
-// (function() {
-//   var ga = document.createElement('script');
-// 	ga.type = 'text/javascript';
-// 	ga.async = true;
-//   ga.src = 'https://ssl.google-analytics.com/ga.js';
-//   var s = document.getElementsByTagName('script')[0];
-// 	s.parentNode.insertBefore(ga, s);
-// })();
 
 
 document.addEventListener('DOMContentLoaded', init);
