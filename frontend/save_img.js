d9f83a66ff8e3a5a4579407a88f769e1d1877965
github username <chang.lim92@gmail.com>
2018-12-09 22:08:33 -0500

    Extension extracting src done. TODO: Post request to database

--- frontend/save_img.js
+++ frontend/save_img.js
@@ -1,1 +1,7 @@
-console.log(clickedUrl);
+console.log("sssup");
+
+console.log(window.localStorage.getItem('clickedone'));
+
+
+var tv = document.getElementById('url');
+tv.value = window.localStorage.getItem('clickedone');
