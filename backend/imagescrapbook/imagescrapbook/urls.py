3b42972d07d61f2c8200a28a24a4b88178bdcb29
Skye Im <skye.im@nyu.edu>
2018-12-11 20:16:56 -0500

    Main page redirects to public gallery

--- backend/imagescrapbook/imagescrapbook/urls.py
+++ backend/imagescrapbook/imagescrapbook/urls.py
@@ -1,8 +1,11 @@
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('gallery'))),
     path('admin', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
     path('api/', include('main.urls')),
