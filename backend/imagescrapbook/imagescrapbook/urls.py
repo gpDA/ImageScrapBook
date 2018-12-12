74f3c43231006781b8320ef2abfcac05b1a496b9
Skye Im <skye.im@nyu.edu>
2018-12-11 20:14:32 -0500

    Remove old gallery from routing

--- backend/imagescrapbook/imagescrapbook/urls.py
+++ backend/imagescrapbook/imagescrapbook/urls.py
@@ -6,13 +6,8 @@ urlpatterns = [
     path('admin', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
     path('api/', include('main.urls')),
-
-
-    path('signup', TemplateView.as_view(template_name="registration/signup.html"), name='signup'),    
     path('login/', include('login.urls')),
-    path('logout', TemplateView.as_view(template_name="registration/logout.html"),name='logout'),
+    path('logout', TemplateView.as_view(template_name="registration/logout.html"), name='logout'),
     path('image/', include('image.urls')),
-    
-    path('api/', include('gallery.urls')),
     path('api/', include('registration.urls')),
 ]
