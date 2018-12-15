ee0b5989dcdc69866c5a4466c6f1598004b01c9c
gp <gp@gps-MacBook-Pro.local>
2018-12-15 14:09:46 -0500

    problem with user "this field is required"

--- extension/save_img.js
+++ extension/save_img.js
@@ -1,48 +1,82 @@
-console.log("sssup");
-console.log("grabbed src: ", window.localStorage.getItem('clickedone'));
-
-var tv = document.getElementById('url');
-tv.value = window.localStorage.getItem('clickedone');
-
-var canvas = document.createElement('canvas');
-var context = canvas.getContext('2d');
-var img = document.getElementById('grabbed_img');
-
-var myImg = new Image();
-var d ;
-img.onload = function() {
-   context.drawImage(myImg, 0, 0);
-   var theData = context.getImageData(0, 0, img.width, img.height);
-   console.log("the Data : ", theData.data);
-};
-
-img.src = tv.value;
-console.log("this not workin tho..why..:", theData.data);
+$(function () {
 
 
+  var tv = document.getElementById('url');
+    
+  var canvas = document.createElement('canvas');
+  var context = canvas.getContext('2d');
+  var img = document.getElementById('grabbed_img');
+  
+  var myImg = new Image();
+  // var d ;
+  // // data is not defined
+  var theData;
+  img.onload = function() {
+     context.drawImage(myImg, 0, 0);
+    //  var theData = context.getImageData(0, 0, img.width, img.height);
+    theData = context.getImageData(0, 0, img.width, img.height);
+    // undefined DATA
+    //  console.log("the Data : ", theData.data);
+  };
+  
+  var canvas = document.createElement('canvas');
+  var context = canvas.getContext('2d');
+  var img = document.getElementById('grabbed_img');
+  
+  // img.src = tv.value //set src of the image
+  
+context.drawImage(img, 10, 10);
 
-const savebtn = document.getElementById('save')
+var dataurl = canvas.toDataURL();
+// base 64 encoded
+// console.log("dataurl: ", dataurl);
 
-savebtn.addEventListener('click', postImage())
 
-const postImage = () => {
-  fetch('https://www.gogle.com', {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
+
+$("#save").on('click', function(e){
+  var title1 = $("#title").val();
+
+  var formData = new FormData() 
+
+  // formData.append('user', '1');
+  formData.append('title', title1);
+  formData.append('image', dataurl);
+  formData.append('extension', 'jpeg')
+
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:8000/api/create",
+    data: formData,
+    // beforeSend : function(xhr){
+    //   var cookie = credentials['COOKIE'];
+    //   console.log('adding cookie'+ cookie);
+    //   xhr.setRequestHeader('Cookie', cookie);
+    // },
+
+    xhrFields: { withCredentials: true },
+    mimeType:"multipart/form-data",
+    processData: false,
+    contentType: false,
+    // crossDomain: true, 
+
+    // dataType: 'json',
+    // contentType: "application/json; charset=utf-8;", // this
+    error: function(errr){
+      console.log(errr);
     },
-    body: JSON.stringify({
-      user: 1, //fake userId. After auth, we will have a "logged in" user Id
-      title: document.getElementById('title').value,
-      image: d,
-      extension: 'jpg', //fake file format. Need to talk to Skye since ours is raw data
-      privacy: false,
-      thumbnail_url: '',
-      imageurl: tv.value,
-    })
+    success: function(data, status, xhr){
+      var token = data["token"];
+      console.log(token)
+      // console.log('successs,,,,' + xhr.getResponseHeader("Set-Cookie"));
+
+      // window.location.href = "http://localhost:8000/gallery/public";
+      // location.replace("http://localhost:8000/gallery/public");
+    }
+
   })
-  .then(res => res.json())
-  .then(res => console.log(res))
-}
-savebtn.addEventListener('click', postImage())
+
+
+})
+
+
+});
